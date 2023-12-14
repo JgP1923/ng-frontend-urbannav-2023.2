363ddecc5c8eb9fece30c453c4e7ac50a3d97ada
Leidy Johana <lj13.az50@gmail.com>
2023-12-13 23:38:20 -0500

    realización del footer

--- src/app/publico/errores/ruta-no-encontrada/ruta-no-encontrada.component.ts
+++ src/app/publico/errores/ruta-no-encontrada/ruta-no-encontrada.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-ruta-no-encontrada',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './ruta-no-encontrada.component.html',
   styleUrl: './ruta-no-encontrada.component.css'
 })
