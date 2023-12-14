fd4bcf291dc129894c6762d84563bb54bf7e660c
Leidy Johana <lj13.az50@gmail.com>
2023-12-13 22:11:31 -0500

    creación del encabezado del landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { EncabezadoComponent } from "./publico/pagina-maestra/encabezado/encabezado.component";
+import { PiePaginaComponent } from './publico/pagina-maestra/pie-pagina/pie-pagina.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, EncabezadoComponent,PiePaginaComponent]
 })
 export class AppComponent {
   title = 'ng-frontend-urbannav-2023.2';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { RutaNoEncontradaComponent } from './publico/errores/ruta-no-encontrada/ruta-no-encontrada.component';
+import { HomeComponent } from './publico/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"home",
+        component: HomeComponent,
+    },
+    {
+        path:"",
+        pathMatch: "full",
+        redirectTo: "/home"
+    },
+    {
+        path:"**",
+        component: RutaNoEncontradaComponent
+    }
+];
